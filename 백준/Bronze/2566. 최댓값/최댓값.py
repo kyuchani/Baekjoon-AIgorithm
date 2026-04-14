48c0f3429c493677614c87d5604e1731c96f8c70
Park KyuChan <xla5193@naver.com>
2026-04-14 19:40:42 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,15 @@
+max_val = -1
+max_row = 0
+max_col = 0
+
+for i in range(9):
+    row = list(map(int, input().split()))
+
+    for j in range(9):
+        if row[j] > max_val:
+            max_val = row[j]
+            max_row = i + 1
+            max_col = j + 1
+
+print(max_val)
+print(max_row, max_col)
